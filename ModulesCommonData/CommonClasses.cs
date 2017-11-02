81353a6c5fa4bf7be807f30c574f098fba6a0265
Alvek <cj9ddrd@gmail.com>
2017-11-03 00:08:10 +0200

    Add project files.

---
+++ ModulesCommonData/CommonClasses.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZedGraph;
+
+namespace NCE.UTscanner.ModulesCommonData
+{
+    public class Channel
+    {
+        private List<Gate> _gates;
+
+        public int ChannelId { get; set; }
+        public List<Gate> Gates
+        {
+            get
+            {
+                return _gates;
+            }
+        }
+
+        public Channel(int gateCount)
+        {
+            _gates = new List<Gate>(gateCount);
+            for (int i = 0; i < gateCount; i++)
+            {
+                _gates.Add(new Gate());
+                _gates[i].Ascans = new List<PointPairList>();
+                _gates[i].GatePoints = new List<PointPair>();
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Gate
+    {
+        public List<PointPair> GatePoints { get; set; }
+        public List<PointPairList> Ascans { get; set; }
+    }
+}
---
+++ ModulesCommonData/Interfaces.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace NCE.UTscanner.ModulesCommonData
+{
+    
+    public interface IModule : IDataflowBlock
+    {
+        string ModuleName { get; }
+    }
+
+    public interface IRawDataInputModule : IRawSplitterTarget, IModule
+    { }
+    /// <summary>
+    /// Интерфейс сплитера конвертированных кооридинат
+    /// </summary>
+    public interface IConvertedSplitterTarget : IDataflowBlock
+    {
+        void PostData(List<Channel> channels);
+    }
+    /// <summary>
+    /// Интерфейс сплитера сырых данных
+    /// </summary>
+    public interface IRawSplitterTarget : IDataflowBlock
+    {
+        void PostData(byte[] raw);
+    }
+    /// <summary>
+    /// Интерфейс сплитера флага для записи координаты лайт барьера
+    /// </summary>
+    public interface ILightBarrierSplitterTarget : IDataflowBlock
+    {
+        void LightBarrierReached(double barrierCoord);
+    }
+}
